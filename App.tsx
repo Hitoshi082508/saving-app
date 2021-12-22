886cec5c9a4383df549d31be7f1deb3e72c540cd
hitoshi082508 <hitoshi082508@gmail.com>
2021-12-22 21:52:49 +0900

    create header

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
 import { Calender } from './src/components/molecules/Calendar';
+import { Header } from './src/components/atoms/Header';
 
 export default function App() {
   return (
     <ScrollView>
+      <Header text="ホーム"/>
       <Calender />
     </ScrollView>
   )
