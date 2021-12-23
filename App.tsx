11de8a796777b47e46cbeda2f247b1a73fa4f830
hitoshi082508 <hitoshi082508@gmail.com>
2021-12-23 20:37:29 +0900

    create LimitInput.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native';
 import { Calender } from './src/components/molecules/Calendar';
 import { Header } from './src/components/atoms/Header';
+import { LimitInput } from './src/components/atoms/LimitInput';
 
 export default function App() {
+  const [text, setText] = useState('');
+  console.log(text);
   return (
     <ScrollView>
       <Header text="ホーム"/>
+      <LimitInput onChangeText={(text) => setText(text)} />
       <Calender />
     </ScrollView>
   );
---
+++ src/components/atoms/LimitInput.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+type Props = {
+  onChangeText: (text: string) => void
+}
+export const LimitInput: React.FC<Props> = ({ onChangeText }) => {
+  return (
+    <TextInput
+      style={styles.container}
+      onChangeText={(text) => onChangeText(text)}
+      placeholder="50000円"
+    />
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    width: 250,
+    height: 40,
+    borderRadius: 5,
+    paddingLeft: 10,
+    shadowColor: '#00000029',
+    shadowOpacity: 6,
+    shadowOffset: {
+      width: 3,
+      height: 0
+    }
+  },
+});
